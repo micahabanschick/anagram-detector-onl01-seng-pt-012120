edf25e04558b74d7f08c028305ef566173e05bf0
Micah Banschick <micha.ban@gmail.com>
2020-02-03 20:03:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(arr)
+    narr = arr.filter do |elem| 
+      elem.length == @word.length && @word.split("").all?{|letter| elem.include?(letter)} == true
+    end 
+    narr
+  end 
+  
+end
