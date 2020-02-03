a9c21ae5b0f97c1f970c819629c6952b586b028c
Micah Banschick <micha.ban@gmail.com>
2020-02-03 20:03:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   
   def match(arr)
     narr = arr.filter do |elem| 
-      elem.length == @word.length && elem.split("").all?{|letter| @word.include?(letter)} == true
+      elem.length == @word.length && @word.split("").all?{|letter| elem.include?(letter)} == true
       #i = 0 
       #while i < elem.length do 
        # elem.include?(@word[i]) == true
